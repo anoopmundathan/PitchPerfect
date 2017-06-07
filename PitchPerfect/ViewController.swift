78d1ba4ba297e59ad8756658daead6f64a58c406
anoopmundathan <anoop.mundathan@gmail.com>
2017-06-07 11:20:55 +0100

    Add viewWillAppear

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("view Will Appear");
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
